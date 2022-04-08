cd4e9046977e3d0a5f47811e6ff6d34a1e457907
Gui Far <gui.far@outlook.com>
2022-04-07 21:48:20 -0300

    first commit

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,42 @@
+import { getSession, useUser, withPageAuthRequired } from '@auth0/nextjs-auth0'
+import { GetServerSideProps } from 'next';
+
+export default function Home() {
+
+    const { user } = useUser();
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <pre>
+                {JSON.stringify(user, null, 2)}
+            </pre>
+
+        </div>
+    )
+
+}
+
+
+export const getServerSideProps = withPageAuthRequired();
+
+//Todo o codigo abaixo é substituido pela linha acima do nest
+/*
+export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
+  
+    const session = getSession(req, res);
+  
+    if(!session) {
+      return {
+        redirect: {
+          destination: '/api/auth/login',
+          permanent: false,
+        }
+      }
+    }
+
+    return {
+        props: {}
+    }
+  }
+*/
